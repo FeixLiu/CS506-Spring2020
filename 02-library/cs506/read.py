48d0416fd217a0ff01d0a3034175ff9f95e25336
YuangLiu <yuangliu@dhcp-wifi-8021x-155-41-27-162.bu.edu>
2020-02-10 17:15:10 -0500

    my version of sim and read, change py37 to py36

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,14 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    file = open(csv_file_path, 'r')
+    rst = []
+    for line in file:
+        line = line.replace('\n', '')
+        line = line.split(',')
+        temp = []
+        for a in line:
+            temp.append(eval(a))
+        rst.append(temp)
+    return rst
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,36 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    rst = 0
+    for i in range(len(x)):
+        rst = rst + (x[i] - y[i]) * (x[i] - y[i])
+    return pow(rst, 0.5)
+
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    rst = 0
+    for i in range(len(x)):
+        if x[i] != y[1]:
+            rst += 1
+    return rst
+
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    set_x = set(x)
+    set_y = set(y)
+    return 1 - len(set_x & set_y) / len(set_x | set_y)
+
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    sum_x = 0
+    sum_y = 0
+    sum_all = 0
+    for i in range(len(x)):
+        sum_x += x[i] * x[i]
+        sum_y += y[i] * y[i]
+        sum_all += x[i] * y[i]
+    return sum_all / (pow(sum_x, 0.5) * pow(sum_y, 0.5))
 
 # Feel free to add more
